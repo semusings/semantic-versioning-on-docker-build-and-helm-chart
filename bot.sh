183289fe564e126c618412f631e4b021eebe60b6
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-06-23 01:17:01 +0545

    fix: add release file

--- bot.sh
+++ bot.sh
@@ -8,12 +8,21 @@ case ${option} in
         -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -V \
         clean install -Drevision="$next_version"
       ;;
-   --publish-docker)
+   --publish)
       # Publish Docker in Github Packages
       DOCKER_PKG=docker.pkg.github.com/bhuwanupadhyay/semantic-versioning-on-docker-build-and-helm-chart/my-service:"$next_version"
       docker login docker.pkg.github.com -u BhuwanUpadhyay -p "$GITHUB_TOKEN"
       docker tag docker.io/bhuwanupadhyay/my-service:"$next_version" "$DOCKER_PKG"
       docker push "$DOCKER_PKG"
+
+      # Publish Helm chart in Github Releases
+      HELM_CHART="my-service-$next_version.tgz"
+      HELM_CHART_FILE_PATH="$(pwd)/target/helm/repo/$FILE_NAME"
+
+      # TODO: yourself
+      # Write script to upload your helm chart in your Artifactory or chart museum.
+
+      echo "Mock publish: $HELM_CHART from $HELM_CHART_FILE_PATH"
       ;;
    *)
       echo "`basename ${0}`:usage: [--prepare] | [--publish]"
